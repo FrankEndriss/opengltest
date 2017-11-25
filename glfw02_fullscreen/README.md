6ac73e53f47b5976d7cd220fd183a5c75181466a
frank <Frank.Endriss@bancos.com>
2017-11-25 08:35:36 +0100

    New example, fullscreen, needs to be implemented.

---
+++ glfw02_fullscreen/README.md
@@ -0,0 +1,2 @@
+Shows howto switch from Windowed mode Window to full screen an back.
+Press "f"-Key to switch, any other key to exit.
---
+++ glfw02_fullscreen/main.c
@@ -0,0 +1,77 @@
+
+#include <GL/gl.h>
+#include <GLFW/glfw3.h>
+
+#include <stdio.h>
+
+
+/** Function called to render the current screen.
+ * Seems to be called 60 times per second.
+ */
+static void render(GLFWwindow *window) {
+	// better dont render anything until we know what to do ;)
+}
+
+static void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %d %s\n", error, description);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    fprintf(stdout, "key_callback, key=%d scancode=%d action=%d mods=%d\n", key, scancode, action, mods);
+
+    if(key==GLFW_KEY_F && action==GLFW_PRESS) { // user pressed "f" key, switches screen modes
+    } else /* exit application */
+    	glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+int main(int argc, char** argv)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    glfwSetErrorCallback(error_callback);
+
+    /* set minimum required glfw version */
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwSetKeyCallback(window, key_callback);
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Make swaping on every hsync. (60fps). Swap-Interval 0 makes up to 4000fps. */
+    glfwSwapInterval(1);
+
+    /* Set the viewport for opengl, use the whole window space. */
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window)) {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+	render(window);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
